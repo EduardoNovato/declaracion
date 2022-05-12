d82d5fa87d3738a2fb1bb4109c06897e5fc27c82
EduardoNovato <ejco2003@gmail.com>
2022-05-12 17:19:44 -0500

    commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const yesBtn = document.querySelector('#yesBtn');
+const yesBtn = document.querySelector("#yesBtn");
+//const noBtn = document.querySelector("#noBtn");
 
-yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+yesBtn.addEventListener("click", function () {
+  alert("Ya somos maridos");
 });
 
-const noBtn = document.querySelector('#noBtn');
+// noBtn.addEventListener("click", function () {
+//   alert("Dar 3 mortales con un oso en la espalda");
+// });
 
-noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('left',randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})
+const noBtn = document.querySelector("#noBtn");
+
+noBtn.addEventListener("mouseover", function () {
+  const randomX = parseInt(Math.random() * 100);
+  const randomY = parseInt(Math.random() * 100);
+  noBtn.style.setProperty("top", randomY + "%");
+  noBtn.style.setProperty("left", randomX + "%");
+  noBtn.style.setProperty("transform", `translate(-${randomX}%,-${randomY}%)`);
+});
